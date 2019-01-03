883dc3805a8f9ef8f71c32f4f92ebe4d28ceaae9
eberPeña <eberg.pena@gmail.com>
2019-01-03 16:41:59 -0600

    Se agreco una variable y una constante

---
+++ VariablesYConstantes.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+
+// Int - Float
+// Una variable se define con var + NOMBREDELAVARIA
+
+var account: Float  = 1000000
+
+
+// Constante
+// Funciones explicitas:
+
+let Name: String = "tuNombre"
+let LastName: String = "tuApellido"
+
+
+
+
