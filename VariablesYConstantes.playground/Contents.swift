1cebdda08c03c3e405ff9b59983ce0e27dbc839d
eberPeña <eberg.pena@gmail.com>
2019-01-03 16:51:58 -0600

    Se deja documentado linea 4, que tipo de datos existen dentro del leguaje

--- VariablesYConstantes.playground/Contents.swift
+++ VariablesYConstantes.playground/Contents.swift
@@ -2,16 +2,26 @@ import Foundation
 
 
 // Int - Float
-// Una variable se define con var + NOMBREDELAVARIA
+// Una variable se define con var + NOMBREDELAVARIABLE
 
 var account: Float  = 1000000
 
 
-// Constante
-// Funciones explicitas:
+
+
+
+//Tipos de Datos  /
+//Booleanos: Solo tienen dos valores falso y verdadero
+//Int (Entero): Se puede
+//Float (Flotante): Son los numeros que no llevan una coma
+//Double (Doble): Es similar al Float, la diferencia es la magnitud de memoria
+//Character (Caracter): Es la representación  de una unica letra dentro de texto
+//String: "Representado para cualquier texto"
 
 let Name: String = "tuNombre"
 let LastName: String = "tuApellido"
+
+
 
 
 
